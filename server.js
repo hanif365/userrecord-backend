61dc4c1ca5a0d5469cef831573255d4ffc90027c
hanif365 <m.a.hanifkhan64@gmail.com>
2023-11-25 15:53:12 +0600

    configure vercel.json

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const routes = require("./routes/user");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI
+
+const MONGODB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@ownerconnect.w4nkndk.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 app.use(cors());
 app.use(express.json());
